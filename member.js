0a47449f09bf0f0959b662ed93c96526b0e955d0
luisarevalo21 <arevaloluis89@yahoo.com>
2024-01-11 21:07:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberHeight = member.clientHeight;
+    skills.style.height = memberHeight + "px";
+  }
+  
